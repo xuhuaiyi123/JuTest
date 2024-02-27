7f7c0a7eb4fddca46ea92a2c3fb246da8e47dba2
xuhuaiyi123 <3102684127@qq.com>
2024-02-27 09:11:50 +0800

    ava 语言实现判断三角形类型程序Triangle，并采用Junit5 或PyTest 编写完成三角形类型程序测试用例，并提交到代码仓库

---
+++ src/main/java/com/example/judemo/JUdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.judemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JUdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/judemo/JUdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.judemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
