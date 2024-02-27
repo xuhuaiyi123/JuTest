9d30fde8b179f35e3c024e6f52f95bac98c5f9d3
xuhuaiyi123 <3102684127@qq.com>
2024-02-27 09:35:37 +0800

    ava 语言实现判断三角形类型程序Triangle，并采用Junit5 或PyTest 编写完成三角形类型程序测试用例，并提交到代码仓库

--- src/test/java/com/example/judemo/TriangleTest.java
+++ src/test/java/com/example/judemo/TriangleTest.java
@@ -2,24 +2,97 @@ package com.example.judemo;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.text.ParseException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TriangleTest {
-
     @Test
-    @DisplayName("输入错误")
-    void parameters_error_test() {
+    @DisplayName(value="不等边三角形")
+    void testTriangle() {
         Triangle triangle = new Triangle();
-        String type = triangle.classify(0, 4, 5);
-        assertEquals("输入错误", type);
-    }
 
-    @Test
-    @DisplayName("不等边三角形")
-    void scalene_test() {
-        Triangle triangle = new Triangle();
         String type = triangle.classify(3, 4, 6);
         assertEquals("不等边三角形", type);
     }
+
+    @Test
+    void equallaterialTriangle() {
+        Triangle triangle = new Triangle();
+
+        String type = triangle.classify(3, 3, 3);
+        assertEquals("等边三角形", type);
+    }
+
+    @Test
+    void notTriangle() {
+        Triangle triangle = new Triangle();
+
+        String type = triangle.classify(3, 3, 6);
+        assertEquals("非三角形", type);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "3,4,6,不等边三角形",
+            "3,3,3,等边三角形",
+            "3,3,6,非三角形"
+    })
+    void paramTriangle(int a, int b,int c,String expected) {
+        Triangle triangle = new Triangle();
+
+        String type = triangle.classify(a, b, c);
+
+        assertEquals(expected, type);
+    }
+    @DisplayName(value="三角形一般边界测试用例")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/三角形一般边界测试用例.csv",numLinesToSkip =1,encoding = "UTF-8")
+    void fileTest(int a, int b,int c,String expected) throws ParseException {
+
+        Triangle triangle=new Triangle();
+
+        String type = triangle.classify(a, b, c);
+
+        assertEquals(expected, type);
+    }
+    @DisplayName(value="三角形健壮性最坏情况测试用例")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/三角形健壮性最坏情况测试用例.csv",numLinesToSkip =1,encoding = "UTF-8")
+    void fileTest1(int a, int b, int c, String expected) throws ParseException {
+
+        Triangle triangle=new Triangle();
+
+        String type = triangle.classify(a, b, c);
+
+        assertEquals(expected, type);
+    }
+    @DisplayName(value="三角形健壮测试用例")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/三角形健壮测试用例.csv",numLinesToSkip =1,encoding = "UTF-8")
+    void fileTest2(int a, int b, int c, String expected) throws ParseException {
+
+        Triangle triangle=new Triangle();
+
+        String type = triangle.classify(a, b, c);
+
+        assertEquals(expected, type);
+    }
+    @DisplayName(value="三角形弱一般等价类测试用例")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/三角形弱一般等价类测试用例.csv",numLinesToSkip =1,encoding = "UTF-8")
+    void fileTest3(int a, int b, int c, String expected) throws ParseException {
+
+        Triangle triangle=new Triangle();
+
+        String type = triangle.classify(a, b, c);
+
+        assertEquals(expected, type);
+    }
+
+
 }
